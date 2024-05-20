2c5588a2499ab34a2e1fcae95dfd60506ef8577a
unknown <joan.sebas.sanpa@gmail.com>
2024-05-20 01:06:50 -0500

    Resueltos Conflicots

--- Api_Sensor/settings.py
+++ Api_Sensor/settings.py
@@ -58,12 +58,12 @@ WSGI_APPLICATION = 'Api_Sensor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # DATABASES = {
 #     'default': {
